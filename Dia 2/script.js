5499a202f09a8ec1a5fbcae4cdf7793717839889
Julio Teixeira <julioteixeirapimont@gmail.com>
2022-03-24 16:19:24 -0300

    dia 2 - impressão e recebimento de valores

---
+++ Dia 2/script.js
@@ -0,0 +1,68 @@
+
+/*
+
+let nome = prompt("Diga seu nome!");
+let idade = prompt ("Diga sua idade!");
+let linguagem = prompt ("Qual a linguagem de programação que você estuda atualmente?")
+alert(`"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`);
+
+
+let resposta = parseInt(prompt (`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`));
+
+switch (resposta){
+
+    case 1:
+        alert ("Muito bom! Continue estudando e você terá muito sucesso.");
+        break;
+    
+    case 2 :
+        alert ("Ahh que pena... Já tentou aprender outras linguagens?")
+        break;
+
+    default:
+       alert ("Por favor, se atenha aos números requeridos.")
+}
+
+// O plano original seria pegar os inputs escrito no HTML, mas o metodo .value não está retornando nada */
+
+
+/*
+let nome = document.getElementById("nome")
+let idade = document.getElementById("idade")
+let linguagem = document.getElementById("linguagem")
+
+function enviar(){
+    nomeValor = nome.value
+    idadeValor = idade.value
+    linguagemValor = linguagem.value
+
+    alert(`"Olá ${nomeValor}, você tem ${idadeValor} anos e já está aprendendo ${linguagemValor}!"`)
+
+
+    document.getElementById("div2").style.display = "flex";
+
+}
+
+function resposta(){
+
+    let resposta = parseInt(document.getElementById("input").value)
+
+    switch(resposta){
+
+        case 1:
+            alert ("Muito bom! Continue estudando e você terá muito sucesso.");
+            break;
+        
+        case 2 :
+            alert ("Ahh que pena... Já tentou aprender outras linguagens?")
+            break;
+    
+        default:
+           alert ("Por favor, se atenha aos números requeridos.")
+
+
+    }
+
+    
+
+} */
